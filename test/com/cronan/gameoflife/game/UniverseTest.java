7ba49f9bb665529a90d7f5f1dcbe78a6c75f7eac
Cody Cronan <codycronberger@gmail.com>
2020-11-21 12:07:24 -0800

    added test folder

--- test/com/cronan/gameoflife/game/UniverseTest.java
+++ test/com/cronan/gameoflife/game/UniverseTest.java
@@ -1,5 +1,20 @@
-import static org.junit.Assert.*;
+package com.cronan.gameoflife.game;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
 
 public class UniverseTest {
+    private Universe universe;
 
+    @Before
+    public void init() {
+        universe = new Universe(10);
+    }
+
+    @Test
+    public void initializeCells() {
+        assertEquals(100, universe.getCells().size());
+    }
 }
