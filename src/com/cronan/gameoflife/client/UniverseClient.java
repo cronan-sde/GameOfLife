6890b3758355d649729cfee22e757c26fbde57db
Cody Cronan <codycronberger@gmail.com>
2020-11-22 13:50:24 -0800

    pausing is working, but there is a bug, need to figure out how to cleanly pause on current generation

--- src/com/cronan/gameoflife/client/UniverseClient.java
+++ src/com/cronan/gameoflife/client/UniverseClient.java
@@ -5,6 +5,8 @@ import com.cronan.gameoflife.game.GameOfLife;
 public class UniverseClient {
 
     public static void main(String[] args) {
-        GameOfLife life = new GameOfLife(100);
+        GameOfLife life = new GameOfLife();
+        Thread game = new Thread(life);
+        game.start();
     }
 }
--- src/com/cronan/gameoflife/game/GameOfLife.java
+++ src/com/cronan/gameoflife/game/GameOfLife.java
@@ -2,20 +2,34 @@ package com.cronan.gameoflife.game;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ItemEvent;
 
-public class GameOfLife extends JFrame {
+public class GameOfLife extends JFrame implements Runnable {
     //Universe
-    private final Universe universe;
+    private Universe universe;
+    //Universe Size
+    private static final int UNIVERSE_SIZE = 50;
     //Labels
     private JLabel generationLabel;
     private JLabel cellsAliveLabel;
     //Buttons
-    private JButton play;
+    private JToggleButton playToggle;
     private JButton reset;
 
+    //button Icons
+//    ImageIcon pauseIcon = new ImageIcon();
+//    ImageIcon playIcon = new ImageIcon();
+//    ImageIcon reloadIcon = new ImageIcon();
 
-    public GameOfLife(int universeSize) {
-        universe = new Universe(universeSize);
+    //lock object monitors the thread to allow for pause/resume and reset
+    private final Object lock = new Object();
+    //controls pausing/resuming of thread
+    private volatile boolean isPaused = false;
+
+
+    public GameOfLife() {
+        universe = new Universe(UNIVERSE_SIZE);
         setTitle("Game of Life");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setSize(600, 600);
@@ -28,44 +42,64 @@ public class GameOfLife extends JFrame {
 
 
     public void start() {
-        //panel for generationLabel and cellsAliveLabel
-        JPanel topPanel = new JPanel();
+        //panel for buttons on left of frame
+        JPanel leftBtnPan = new JPanel();
+        //panel for labels on left
+        JPanel leftPan = new JPanel();
+        //main panel to hold buttons and labels
+        JPanel mainPan = new JPanel();
 
-        //JLabels
+        //pause/resume toggle button and reset button
+        playToggle = new JToggleButton("Pause");
+        playToggle.setName("PlayToggleButton");
+        reset = new JButton("Reset");
+        reset.setName("ResetButton");
+
+
+        playToggle.addItemListener(this::setPlayToggle); //allows for pause/resume operation
+        reset.addActionListener(this::reset); //resets the universe to new random universe
+
+
+        //add buttons to panel
+        leftBtnPan.add(playToggle);
+        leftBtnPan.add(reset);
+        leftBtnPan.setLayout(new BoxLayout(leftBtnPan, BoxLayout.X_AXIS)); //sets buttons next to each other
+        leftBtnPan.setAlignmentX(Component.CENTER_ALIGNMENT);
+
+        //JLabels generation and alive labels
         generationLabel = new JLabel();
-        generationLabel.setName("genLabel");
+        generationLabel.setName("GenerationLabel");
         cellsAliveLabel = new JLabel();
-        cellsAliveLabel.setName("cellsAlive");
+        cellsAliveLabel.setName("AliveLabel");
+        //update label text
         updateLabels();
 
-        //Buttons
-        //TODO: add functionality to play and pause game
-        play = new JButton("Play");
 
-        //TODO: add functionality to reset the entire game on button press
-        reset = new JButton("Reset");
+        //adding labels to left panel
+        leftPan.add(generationLabel);
+        leftPan.add(cellsAliveLabel);
+        leftPan.setLayout(new BoxLayout(leftPan, BoxLayout.Y_AXIS)); //setting box layout to stack labels vertical
+        leftPan.setAlignmentX(Component.CENTER_ALIGNMENT);
 
-        //add labels to top panel
-        topPanel.add(generationLabel);
-        topPanel.add(cellsAliveLabel);
-        topPanel.add(play);
-        topPanel.add(reset);
-        topPanel.setLayout(new BoxLayout(topPanel, BoxLayout.Y_AXIS)); //stacking labels vertically
+        //Adding button panel and label panel to main panel
+        mainPan.add(leftBtnPan);
+        mainPan.add(leftPan);
 
-        getContentPane().add(BorderLayout.WEST, topPanel); // adding topPanel to top of frame
-        setVisible(true);
+        mainPan.setLayout(new BoxLayout(mainPan, BoxLayout.Y_AXIS)); //Stacking panels vertically
+        mainPan.setBorder(BorderFactory.createEmptyBorder(20,5,5,5)); //setting spacing
 
+
+        getContentPane().add(BorderLayout.WEST, mainPan); //adding main panel to the left side of frame
+        setVisible(true); //setting frame to be visible
     }
 
     public void displayCurrentUniverse() {
-        int universeLength = universe.getCurrentUniverse().length;
         char[][] universeGrid = universe.getCurrentUniverse();
-        System.out.println(universeLength);
         JPanel cellPanel = new JPanel();
-        cellPanel.setLayout(new GridLayout(universeLength, universeLength));
+        cellPanel.setLayout(new GridLayout(UNIVERSE_SIZE, UNIVERSE_SIZE));
 
         for (char[] cell : universeGrid) {
-            for (int j = 0; j < universeLength; j++) {
+            for (int j = 0; j < UNIVERSE_SIZE; j++) {
                 JButton cellBtn = new JButton();
                 cellBtn.setSize(1,1);
                 cellBtn.setEnabled(false);
@@ -89,12 +123,32 @@ public class GameOfLife extends JFrame {
         cellsAliveLabel.setText("Alive: " + Universe.getCellsAlive());
     }
 
+//    public void evolution() {
+//        displayCurrentUniverse();
+//        while (true) {
+//            universe.nextGeneration();
+//            slowDisplay();
+//            displayCurrentUniverse();
+//        }
+//    }
+
     public void evolution() {
-        displayCurrentUniverse();
-        while (true) {
-            universe.nextGeneration();
-            slowDisplay();
-            displayCurrentUniverse();
+        allowPause();
+        slowDisplay();
+        universe.nextGeneration();
+    }
+
+    //allow thread to be paused using lock object
+    public void allowPause() {
+        synchronized (lock) {
+            while (isPaused) {
+                try {
+                    lock.wait();
+                }
+                catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
         }
     }
 
@@ -107,4 +161,34 @@ public class GameOfLife extends JFrame {
         }
     }
 
+    public void setPlayToggle(ItemEvent e) {
+        if (playToggle.isSelected()) {
+            playToggle.setText("Play");
+            isPaused = true;
+        }
+        else {
+            playToggle.setText("Pause");
+            isPaused = false;
+        }
+        synchronized (lock) {
+            lock.notify();
+        }
+    }
+
+    public void reset(ActionEvent e) {
+        universe = new Universe(UNIVERSE_SIZE);
+        isPaused = false;
+        playToggle.setSelected(false);
+        synchronized (lock) {
+            lock.notify();
+        }
+    }
+
+    @Override
+    public void run() {
+        while (true) {
+            displayCurrentUniverse();
+            evolution();
+        }
+    }
 }
